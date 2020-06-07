f39455d4653d3de73ac507496f6c9ca422824180
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-06-07 17:47:31 +0530

    for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import data from "./data";
+import path from "path";
 import userRoute from "./Route/Route";
 const bodyParser = require("body-parser");
 import productRoute from "./Route/productRoute";
@@ -21,7 +21,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-
+app.use(express.static(path.join(__dirname, "/../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
 });
